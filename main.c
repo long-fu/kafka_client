9216588335d7c640deeb7d0f4bf689abffadd882
longfu <haoshuai9291@163.com>
2023-07-05 11:15:21 +0800

    本地测试

--- main.c
+++ main.c
@@ -4,16 +4,18 @@
 
 int consumer(const char *brokers, const char *groupid, char **topics, int topic_cnt);
 
+char g_body[] = "dlkasjdlkaskldjlaksjdlkasjllllklasjdlkasjdljaslkdjlkasdkljaskldjklasjdljaskldjalskjddddddddddddddddddddaaaaaad2933333333480239840820348jlkdsjffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffsdlak;lskd;lkasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssffffffffffffffffffffffffffffffffffffff\r\n192.168.2.1\r\n8080";
+char g_body_buf[2048] = {0}; 
 int main(int argc, char const *argv[])
 {
         // char body[] = "你好\r\n192.168.2.1\r\n8080";
-        // char g_body_buf[32] = {0}; 
-        // char des_ip[16] = {0};
-        // char des_port[8] = {8};
-        // sscanf(body,"%s\r\n%s\r\n%s",g_body_buf, des_ip, des_port);
-        // printf("port %s \n", des_port);
-        // int port = atoi(des_port);
-        // printf("%s \n%s\n%d\n",g_body_buf,des_ip,port);
+        
+        char des_ip[16] = {0};
+        char des_port[8] = {8};
+        sscanf(g_body,"%s\r\n%s\r\n%s",g_body_buf, des_ip, des_port);
+        printf("port %s \n", des_port);
+        int port = atoi(des_port);
+        printf("%s \n%s\n%d\n",g_body_buf,des_ip,port);
         char *topics[1] = {"alarm-events"};
-        return consumer("localhost:9092","console-consumer-62319",topics,1);
+        // return consumer("localhost:9092","console-consumer-62319",topics,1);
 }
