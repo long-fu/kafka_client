66953931deb5ee1862923550a7f546e7b7cd19fa
Your Name <you@example.com>
2023-07-05 19:52:25 +0800

    buf len

--- http.c
+++ http.c
@@ -176,11 +176,11 @@ int http_send(const char *body, size_t length)
     char *header = g_header_buf;
 
     char cl[32] = {0};
-    sprintf(cl, "%ld", length);
+    sprintf(cl, "%ld", strlen(g_body_buf));
 
     strcat(header, "POST /api/smartbox/AlarmPost HTTP/1.1\r\n");
     strcat(header, "Cache-Control:no-cache\r\n");
-    strcat(header, "Connection:close\r\n");
+    strcat(header, "Connection:Keep-Alive\r\n");
     strcat(header, "Accept-Encoding:gzip,deflate,br\r\n");
     strcat(header, "Accept:*/*\r\n");
     strcat(header, "Content-Type:application/json\r\n");
@@ -189,66 +189,13 @@ int http_send(const char *body, size_t length)
     strcat(header, "Content-Length:");
     strcat(header, cl);
     strcat(header, "\r\n\r\n");
-    //body的值为post的数据
-    strcat(header, body);
-    strcat(header, "\r\n\r\n");
-//    printf("%s\n",head);
 
-//     strcat(header, "POST /api/smartbox/AlarmPost HTTP/1.1");
-//     strcat(header, "\r\n");
-
-//     strcat(header, "Cache-Control: no-cache");
-//     strcat(header, "\r\n");
-
-<<<<<<< HEAD
-//     strcat(header, "Connection: close");
-//     strcat(header, "\r\n");
-=======
-    strcat(header, "Connection: keep-alive");
-    strcat(header, "\r\n");
->>>>>>> origin/main
-
-//     strcat(header, "Accept-Encoding: gzip,deflate,br");
-//     strcat(header, "\r\n");
-
-//     strcat(header, "Accept: */*");
-//     strcat(header, "\r\n");
-
-//     strcat(header, "Content-Type: application/json");
-//     strcat(header, "\r\n");
-
-//     strcat(header, "User-Agent: Mozilla/5.0");
-//     strcat(header, "\r\n");
-
-<<<<<<< HEAD
-// // strcat(header, "User-Agent:Mozilla/5.0\r\n");
-//     // strcat(header, "Accept-Encoding:gzip,deflate,br\r\n");
-//     // strcat(header, "Accept:*/*\r\n");
-//     // strcat(header, "Content-Type:application/json\r\n");
-=======
-    // strcat(header, "Host: 128.0.0.1\r\n");
->>>>>>> origin/main
-
-//     // strcat(header, "host:22.11.123.78:80\r\n");
-//     // strcat(header, "\r\n");
-
-//     strcat(header, cl);
-//     strcat(header, "\r\n");
-//     strcat(header, "\r\n");
-
-<<<<<<< HEAD
-//     strcat(header, body_buf1);
-//     strcat(header, "\r\n");
-//     strcat(header, "\r\n");
-=======
     strcat(header, g_body_buf);
-    strcat(header, "\r\n");
-    strcat(header, "\r\n");
->>>>>>> origin/main
+strcat(header, "\r\n\r\n");
 
     struct sockaddr_in *dest_addr;
 
-    // printf("mesgbodu\n%s\n", header);
+    printf("mesgbodu\n%s\n", header);
 
     int fd = socket_create("wlp0s20f3", "192.168.2.4", 7890, des_ip, des_port, dest_addr);
 
@@ -285,17 +232,10 @@ int http_send(const char *body, size_t length)
     while ((n = read(fd, temp, 512)) > 0)
     {
         // 接收的数据很小
-<<<<<<< HEAD
-        // printf("接收[%d]=========\n",ri);
-        printf("%s\n",temp);
-        strlncat(g_read_buf, strlen(g_read_buf), temp, n);
-        memset(temp, 0x0, 512);
-=======
         printf("接收[%d]=========\n", ri);
         printf("%s\n", temp);
-        // strlncat(g_read_buf, strlen(g_read_buf), temp, n);
-        // memset(temp, 0x0, 512);
->>>>>>> origin/main
+        strlncat(g_read_buf, strlen(g_read_buf), temp, n);
+        memset(temp, 0x0, 512);
     }
     printf("接收的消息 %s \n", g_read_buf);
     // TODO: 解析接收的消息
