ade41ca4e93064a3120ac3a6d07293dffe314b41
longfu <haoshuai9291@163.com>
2023-07-05 16:42:06 +0800

    去掉网卡绑定

--- soc.c
+++ soc.c
@@ -73,32 +73,32 @@ int socket_create(const char *ethx, const char *src_ip, int src_port, const char
   // memset(&src_addr, 0, sizeof(src_addr));
   memset(dest_addr, 0, sizeof(struct sockaddr_in));
 
-  inet_pton(AF_INET, src_ip, &src_addr.sin_addr.s_addr);
+  // inet_pton(AF_INET, src_ip, &src_addr.sin_addr.s_addr);
 
-  src_addr.sin_family = AF_INET;
-  src_addr.sin_port = htons(src_port);
-  src_addr.sin_addr.s_addr = inet_addr(src_ip);
+  // src_addr.sin_family = AF_INET;
+  // src_addr.sin_port = htons(src_port);
+  // src_addr.sin_addr.s_addr = inet_addr(src_ip);
 
-  ret = bind(fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
-  if (ret < 0)
-  {
-    printf("bind error!!!\n");
-    close(fd);
-    return -1;
-  }
+  // ret = bind(fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+  // if (ret < 0)
+  // {
+  //   printf("bind error!!!\n");
+  //   close(fd);
+  //   return -1;
+  // }
 
-  struct ifreq nif;
-  strcpy(nif.ifr_name, ethx);
-  if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, (char *)&nif, sizeof(nif)) < 0)
-  {
-    close(fd);
-    printf("bind interface fail, errno: %d \r\n", errno);
-    return -1;
-  }
-  else
-  {
-    printf("bind interface success \r\n");
-  }
+  // struct ifreq nif;
+  // strcpy(nif.ifr_name, ethx);
+  // if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, (char *)&nif, sizeof(nif)) < 0)
+  // {
+  //   close(fd);
+  //   printf("bind interface fail, errno: %d \r\n", errno);
+  //   return -1;
+  // }
+  // else
+  // {
+  //   printf("bind interface success \r\n");
+  // }
 
   dest_addr->sin_family = AF_INET;
   dest_addr->sin_addr.s_addr = inet_addr(dest_ip);
@@ -107,22 +107,22 @@ int socket_create(const char *ethx, const char *src_ip, int src_port, const char
   if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     printf("1.socket 网络连接失败,本线程即将终止[socket error]错误代码是%d, 错误信息是'%s'\n", errno, strerror(errno));
-    setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, dest_addr, sizeof(struct sockaddr_in));
+    // setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, dest_addr, sizeof(struct sockaddr_in));
     return -1;
   }
 
-  if (inet_pton(AF_INET, dest_ip, &dest_addr->sin_addr) <= 0)
-  {
-    printf("2.inet 网络连接失败,本线程即将终止[inet_pton error]错误代码是%d, 错误信息是'%s'\n", errno, strerror(errno));
-    setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, dest_addr, sizeof(struct sockaddr_in));
-    close(fd);
-    return -1;
-  }
+  // if (inet_pton(AF_INET, dest_ip, &dest_addr->sin_addr) <= 0)
+  // {
+  //   printf("2.inet 网络连接失败,本线程即将终止[inet_pton error]错误代码是%d, 错误信息是'%s'\n", errno, strerror(errno));
+  //   setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, dest_addr, sizeof(struct sockaddr_in));
+  //   close(fd);
+  //   return -1;
+  // }
 
   if (connect(fd, dest_addr, sizeof(struct sockaddr_in)) < 0)
   {
     printf("3.connect 服务器失败[connect error]错误代码是%d, 错误信息是'%s'\n", errno, strerror(errno));
-    setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, dest_addr, sizeof(struct sockaddr_in));
+    // setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, dest_addr, sizeof(struct sockaddr_in));
     close(fd);
     return -1;
   }
@@ -136,6 +136,6 @@ int socket_create(const char *ethx, const char *src_ip, int src_port, const char
 
 int socket_destroy(int fd, struct sockaddr_in *addr)
 {
-  setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, addr, sizeof(struct sockaddr_in));
+  // setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, addr, sizeof(struct sockaddr_in));
   close(fd);
 }
